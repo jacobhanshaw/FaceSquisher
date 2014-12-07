8b6757db78a8b029d65270d046a0bfbb769bd8da
Jacob Hanshaw <guitarboy667@aim.com>
2014-12-06 20:04:33 -0600

    Project

---
+++ META2/Assets/Scripts/PinchScript.cs
@@ -0,0 +1,48 @@
+﻿using Meta;
+using UnityEngine;
+using System.Collections;
+
+public class PinchScript : MonoBehaviour
+{
+		private const float PINCH_DEBOUNCE_TIME = 0.5f;
+
+		private float leftPinchDebounce = -1.0f;
+		private float rightPinchDebounce = -1.0f;
+		private bool leftPinchReleased = true;
+		private bool rightPinchReleased = true;
+		public GameObject pinchPrefab;
+
+		// Use this for initialization
+		void Start ()
+		{
+				Meta.InputIndicators.Instance.handCloud = true;
+				//	pinchPrefab = GameObject.CreatePrimitive (PrimitiveType.Cube);
+				//		pinchPrefab.transform.localScale = new Vector3 (0.01f, 0.01f, 0.01f);
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				CheckForPinch (Hands.left.gesture, ref leftPinchReleased, ref leftPinchDebounce);
+				CheckForPinch (Hands.right.gesture, ref rightPinchReleased, ref rightPinchDebounce);
+		}
+
+		private void CheckForPinch (Gesture gesture, ref bool pinchReleased, ref float pinchDebounce)
+		{
+				if (gesture.type != Meta.MetaGesture.PINCH) {
+						pinchDebounce += Time.deltaTime;
+						if (pinchDebounce > PINCH_DEBOUNCE_TIME)
+								pinchReleased = true;
+				} else {
+						if (pinchReleased && gesture.isValid) {
+								pinchReleased = false;
+								Transform init = Instantiate (pinchPrefab.transform, gesture.position, Quaternion.identity) as Transform;
+								if (init.name.Equals ("splash(Clone)"))
+										init.rotation = Camera.main.transform.rotation;
+		
+						}
+						pinchDebounce = 0.0f;
+				}
+				
+		}
+}
---
+++ README.txt
@@ -0,0 +1,5 @@
+Face Squisher Meta App
+
+Pinching creates a splash effect. The basic idea was to squish peoples heads as a gag, but the hand tracking is still a bit wonky, so the position isn't very accurate.
+
+I just borrowed the splash effect from Unity's 2D Demo, so it's a bit more like a splash in the London river than a head explosion.
